c3fd78e21c5290caf96e10889132d31164ff1c79
unknown <narjessheikhali@gmail.com>
2022-05-29 04:20:51 +0430

    add style to home page

--- monster/src/component/home.jsx
+++ monster/src/component/home.jsx
@@ -3,15 +3,16 @@ import {Container} from 'react-bootstrap'
 export const Home=()=>{
     return(
         <div className="home">
-<div>
-            welcome to this page
-            welcome to this page
-            welcome to this page
-            welcome to this page
-            welcome to this page
-            welcome to this page
-            welcome to this page
-            welcome to this page
+<div className='row'>
+    <div className='col'>
+           <a href='/'>
+               <img src='https://images.unsplash.com/photo-1540845692348-b9d2bc813a63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGZsb3dlciUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'></img>
+           </a>
+           </div>
+           <div className='col'>
+           <p>welcome to this page</p>
+           </div>
+         
             </div>
         </div>
     )
